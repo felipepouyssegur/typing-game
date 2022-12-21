500d72dbb337acb780a738b55748e0b8b0e1ae88
Felipe Pouysségur <pipemdp@hotmail.com>
2022-12-21 16:11:39 -0300

    proyecto finalizado

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,9 +6,10 @@ const Loading = () => {
     return (
         <div className='loading'>
             <img src={Imagen} alt="loader" />
-            <p className='text'> Por favor aguardar un instante, la aplicación está cargando...</p>
-            <p className='text'>Recordá tener <span className='caps'>CAPS LOCK</span> desactivado :)</p>
-            <p className='text'>Activar aceleración por hardware en configuración del navegador.</p>
+            <p className='text'> Por favor aguardar un instante, la aplicacion esta cargando...</p>
+            <p className='text'>Recorda tener <span className='caps'>CAPS LOCK</span> desactivado :)</p>
+            <p className='text'>Activar aceleracion por hardware en configuracion del navegador.</p>
+            <p className='text'>Permanecer en la pantalla de carga para evitar errores.</p>
         </div>
     )
 }
